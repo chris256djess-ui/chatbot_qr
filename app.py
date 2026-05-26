24c21ddf1ed36e13c5f95563f06b3af1a0bc8a91
Christian <chris.256djess@gmail.com>
2026-05-26 00:27:50 -0600

    Actualizacion5

--- app.py
+++ app.py
@@ -1,10 +1,41 @@
 from flask import Flask, render_template, request, jsonify
+from openpyxl import Workbook, load_workbook
+import os
 
 app = Flask(__name__)
+
+ARCHIVO = "usuarios.xlsx"
+
+# 🔹 crear archivo si no existe
+if not os.path.exists(ARCHIVO):
+    wb = Workbook()
+    ws = wb.active
+    ws.title = "Usuarios"
+    ws.append(["ID", "Nombre"])
+    wb.save(ARCHIVO)
+
+def guardar_en_excel(nombre):
+    wb = load_workbook(ARCHIVO)
+    ws = wb.active
+
+    # calcular ID
+    nuevo_id = ws.max_row
+
+    ws.append([nuevo_id, nombre])
+    wb.save(ARCHIVO)
 
 @app.route("/")
 def inicio():
     return render_template("index.html")
+
+@app.route("/guardar_usuario", methods=["POST"])
+def guardar_usuario():
+    data = request.json
+    nombre = data["nombre"]
+
+    guardar_en_excel(nombre)
+
+    return jsonify({"mensaje": "usuario guardado en excel"})
 
 @app.route("/mensaje", methods=["POST"])
 def mensaje():
@@ -12,7 +43,7 @@ def mensaje():
     texto = data["mensaje"]
 
     if texto.lower() == "hola":
-        respuesta = "Hola Chris"
+        respuesta = "Hola"
     else:
         respuesta = "No entiendo el mensaje"
 
